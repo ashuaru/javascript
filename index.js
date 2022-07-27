ad26c8494686bb23d9f286ef7e56ec2f58cbe154
ashuaru <29342444+ashuaru@users.noreply.github.com>
2022-07-27 18:07:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-var userplay = prompt("Do you choose rock, paper or scissors?");
-var computerplay = Math.random();
-if (computerplay < 0.34) {
-    computerplay = "rock";
-} else if(computerplay <= 0.67) {
-    computerplay = "paper";
-} else {
-    computerplay = "scissors";
-}
-
-var compare = function(userplay, computerplay) {
-    if(userplay === computerplay) {
-    console.log("The result is a tie!");
-}
-if(userplay === "rock") {
-    if(computerplay === "scissors") {
-        console.log("rock wins");
+function computerPlay() {
+    let random = Math.random();
+    if (random <= 0.3333) {
+      return "paper";
+    } else if (random >= 0.6666) {
+      return "rock";
     } else {
-        console.log("paper wins");
+      return "scissors";
     }
-}
-if(userplay === "paper") {
-    if(computerplay === "rock") {
-        console.log("paper wins");
-    } else {
-        if(computerplay === "scissors") {
-            console.log("scissors wins");
+  }
+  
+  
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+      if (computerSelection === "paper") {
+        computerScore++;
+        return lose;
+      } else if (computerSelection === "rock") {
+        return tie;
+      } else {
+        userScore++;
+        return win;
+      }
     }
-}
-if(userplay === "scissors") {
-    if(computerplay === "rock") {
-        console.log("rock wins");
-    } else {
-        if(computerplay === "paper") {
-            console.log("scissors wins");
-        }
+  
+    if (playerSelection.toLowerCase() === "scissors") {
+      if (computerSelection === "paper") {
+        userScore++;
+        return win;
+      } else if (computerSelection === "rock") {
+        computerScore++;
+        return lose;
+      } else {
+        return tie;
+      }
     }
-}
-}
-};
-console.log("User play: " + userplay);
-console.log("Computer play: " + computerplay);
-compare(userplay, computerplay);
+  
+    if (playerSelection.toLowerCase() === "paper") {
+      if (computerSelection === "paper") {
+        return tie;
+      } else if (computerSelection === "rock") {
+        userScore++;
+        return win;
+      } else {
+        computerScore++;
+        return lose;
+      }
+    }
+  }
+  
+  
+  let userScore = parseInt(0);
+  let computerScore = parseInt(0);
+  let win = "You win"
+  let lose = "You lose"
+  let tie = "It is a tie"
+  
+  for(var i=0;i<5;i++){
+    let playerSelection = prompt("Pick a move");
+    const computerSelection = computerPlay()
+    console.log(playRound(playerSelection, computerSelection))
+    console.log("your score = " + userScore);
+    console.log("Computer's score = " + computerScore);
+  }
+  
